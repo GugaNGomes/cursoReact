9fbd991ec379234404a8cb5ad9b16fd58107632a
Gustavo Gomes do Nascimento <gustavo.nascimento@postall-log.com.br>
2025-07-10 14:38:19 -0300

    segundo commit

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,6 +1,7 @@
 import { ChevronLeftIcon } from "lucide-react";
 import { Navigate, useNavigate, useParams } from "react-router-dom";
 import Title from "../components/Title.jsx";
+import Butoon from "../components/button.jsx";
 
 function TaskPage() {
   const { title, description } = useParams();
@@ -10,12 +11,12 @@ function TaskPage() {
     <h1 className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="W-[500px] space-y-4">
         <div className="flex; justify-center relative">
-          <button
+          <Butoon
             onClick={() => navigate(-1)}
             className="absolute left-0 top-0 p-2 bg-slate-400 rounded-md text-white"
           >
             <ChevronLeftIcon></ChevronLeftIcon>
-          </button>
+          </Butoon>
           <Title>Detalhes da Tarefa</Title>
         </div>
 
